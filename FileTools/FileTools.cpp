d1e80686b52dc42bc1bc9c65417da6bf532d3af0
Zhu-jiatong <64255832+Zhu-jiatong@users.noreply.github.com>
2022-04-05 22:45:19 +0800

    Better CLI

--- FileTools/FileTools.cpp
+++ FileTools/FileTools.cpp
@@ -19,6 +19,7 @@ chrono::steady_clock::time_point nowT;
 
 void fCipher(string file, int code);
 void showBar();
+void hideCursor();
 
 int main(int argc, char* argv[])
 {
@@ -31,6 +32,8 @@ int main(int argc, char* argv[])
 	thread t1(fCipher, rawPath, key);
 	showBar();
 	t1.join();
+	cout << endl << "complete!" << ends;
+	Sleep(3000);
 	return 0;
 }
 
@@ -46,7 +49,8 @@ void fCipher(string file, int code) {
 	if (file.substr(file.find_last_of("\\") + 1, file.length()).find("encrypted_") != file.npos)
 	{
 		string outPath = file.substr(0, file.find_last_of("\\") + 1) + file.substr(file.find_last_of("\\") + 1, file.length()).replace(file.substr(file.find_last_of("\\") + 1, file.length()).find("encrypted_"), 10, "");
-		cout << "OUTPUT FILE PATH: " << outPath << endl;
+		cout << "OUTPUT FILE PATH: " << outPath << endl << endl;
+		hideCursor();
 		ofstream fout(outPath, ios::binary);
 		startT = chrono::steady_clock::now();
 		ready = true;
@@ -65,23 +69,21 @@ void fCipher(string file, int code) {
 	else
 	{
 		string outPath = file.substr(0, file.find_last_of("\\") + 1) + "encrypted_" + file.substr(file.find_last_of("\\") + 1, file.length());
-		cout << "OUTPUT FILE PATH: " << outPath << endl;
+		cout << "OUTPUT FILE PATH: " << outPath << endl << endl;
 		ofstream fout(outPath, ios::binary);
+		hideCursor();
 		startT = chrono::steady_clock::now();
 		ready = true;
 		while (fin.get(rawTemp))
 		{
 			intTemp = rawTemp;
-			//cout << intTemp << endl;
 			intTemp -= code;
-			//cout << (char)intTemp;
 			fout.put((char)intTemp);
 			progress = progress + 1;
 		}
 		fout.close();
 	}
 	fin.close();
-	//system("pause");
 	done = true;
 }
 
@@ -97,16 +99,23 @@ void showBar() {
 		nowT = chrono::steady_clock::now();
 		chrono::steady_clock::time_point beginT = startT;
 		chrono::steady_clock::duration timeSpan = nowT - beginT;
-		//long timeVal = timeSpan.count()* chrono::milliseconds::period::num / chrono::milliseconds::period::den;
+
 		auto timeVal = chrono::duration_cast<chrono::seconds>(nowT - beginT).count();
 		auto rate = progress / timeVal;
 		auto tLeft = (encrypt.max - progress) / rate;
 		int hLeft = tLeft / 3600;
 		auto mLeft = ((long)tLeft % 3600) / 60;
 		auto sLeft = ((long)tLeft % 3600) % 60;
-		cout << "TIME LEFT: " << hLeft << "hrs " << mLeft << "mins " << sLeft << "secs" << " | SPEED: " << setprecision(1) << fixed << rate / 1000 << "KBps" << setw(1);
-		COORD cPos{ 0, 4 };
+		cout << "TIME REMAINING: " << hLeft << "hrs " << mLeft << "mins " << sLeft << "secs" << " | SPEED: " << setprecision(1) << fixed << rate / 1000 << "KBps" << setw(1);
+		COORD cPos{ 0, 5 };
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cPos);
 		this_thread::sleep_for(chrono::milliseconds(125));
 	}
+}
+
+void hideCursor() {
+	CONSOLE_CURSOR_INFO cursor;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+	cursor.bVisible = false;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
 }
